b00ab8a34e308ccd93726a6288b4ee01e0a65561
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-05-09 17:19:57 +1000

    add generic type demonstration

---
+++ src/generic-types/index.ts
@@ -0,0 +1,5 @@
+const showType = <T>(arg: T): void => {
+  console.log(`typeof ${arg} - ${typeof arg}`);
+};
+
+export default showType;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // import showType from './intersection-types/index';
-import showType from './union-types/index';
+// import showType from './union-types/index';
+import showType from './generic-types/index';
 
 // Demonstrates Intersection Types
 // showType({ id: 1, left: 'test', right: 'test' });
 
 // Demonstrates Union Types
-showType('test');
-showType(2);
+// showType('test');
+// showType(2);
+
+// Demonstrates Generic Types
+showType(1);
